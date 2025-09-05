0db77cfd781f385377cab08e6b0d88fe5388dfff
Filipe Bispo Carneiro <96755431+MonoDevPro@users.noreply.github.com>
2025-09-04 21:44:26 -0400

    refactor: pass Input by reference in MoveIntent for performance optimization

--- Simulation.Application/DTOs/CharacterIntents.cs
+++ Simulation.Application/DTOs/CharacterIntents.cs
@@ -6,5 +6,5 @@ namespace Simulation.Application.DTOs;
 public record struct EnterIntent(int CharId);
 public record struct ExitIntent(int CharId);
 public record struct AttackIntent(int CharId);
-public record struct MoveIntent(int CharId, Input Input);
+public record struct MoveIntent(int CharId, in Input Input);
 public record struct TeleportIntent(int CharId, int MapId, Position Pos);
